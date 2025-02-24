06ad7c48abe924def96ed49e228a2153c13f31b9
Luana Silva <luana.dossantos@zup.com.br>
2025-02-24 12:44:12 -0300

    chore: Initial commit

---
+++ src/main/java/com/catalisa/banco_com_jwt_e_security/BancoComJwtESecurityApplication.java
@@ -0,0 +1,13 @@
+package com.catalisa.banco_com_jwt_e_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoComJwtESecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoComJwtESecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalisa/banco_com_jwt_e_security/BancoComJwtESecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalisa.banco_com_jwt_e_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoComJwtESecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
